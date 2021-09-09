d6961ff3b57e37517d2da8a1524d645bd11ba94b
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-09-09 22:00:15 +0200

    add a fake test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,18 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+-- |
+-- Module      :  Spec
+-- Description :  Shrinkage test suite
+-- Copyright   :  (c) 2021 Dominik Schrempf
+-- License     :  GPL-3.0-or-later
+--
+-- Maintainer  :  dominik.schrempf@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Creation date: Thu Sep  9 21:59:03 2021.
+module Main
+  ( main
+  )
+where
+
+main :: IO ()
+main = return ()
