9104494d4993d24dc83db2e614cf2948c07ca699
Dominik Schrempf <dominik.schrempf@gmail.com>
2021-09-09 15:04:06 +0200

    fill in the last functions

--- src/Statistics/Shrinkage.hs
+++ src/Statistics/Shrinkage.hs
@@ -14,6 +14,7 @@ module Statistics.Shrinkage
   )
 where
 
+import Data.Foldable
 import qualified Numeric.LinearAlgebra as L
 
 -- | Shrinkage based covariance estimator.
@@ -28,36 +29,50 @@ covariance ::
   L.Matrix Double
 covariance xs = undefined
 
--- Adjusted Frobenius norm (p 376).
+-- Inner product for symmetric matrices based on an adjusted Frobenius norm (p
+-- 376).
+--
+-- NOTE: This function is commutative (and therefor qualified as an inner
+-- product) for symmetric matrices only, and not in the general case.
 frobenius :: L.Matrix Double -> L.Matrix Double -> Double
 frobenius xs ys
   | xsRows /= xsCols = error "frobenius: Left matrix is not square."
   | ysRows /= ysCols = error "frobenius: Right matrix is not square."
   | xsRows /= ysRows = error "frobenius: Matrices have different size."
   | otherwise = recip (fromIntegral xsRows) * trace (xs L.<> L.tr' ys)
-  where xsRows = L.rows xs
-        xsCols = L.cols xs
-        ysRows = L.rows ys
-        ysCols = L.cols ys
-        trace = L.sumElements . L.takeDiag
+  where
+    xsRows = L.rows xs
+    xsCols = L.cols xs
+    ysRows = L.rows ys
+    ysCols = L.cols ys
+    trace = L.sumElements . L.takeDiag
 
--- Estimator of mu (lemma 3.2).
+-- Estimator of mu (Lemma 3.2).
 muE ::
+  -- Identity matrix.
+  L.Matrix Double ->
   -- Sample covariance matrix.
   L.Matrix Double ->
   Double
-muE sigma = undefined
+-- NOTE: Strictly speaking, 'flip' is required here to fulfill the formulas
+-- given in Lemma 3.2. However, both matrices are symmetric and so, it can be
+-- omitted. See 'frobenius'.
+muE = frobenius
 
--- Estimator of d2 (lemma 3.3).
+-- Estimator of d2 (Lemma 3.3).
 d2E ::
+  -- Identity matrix.
+  L.Matrix Double ->
   -- Sample covariance matrix.
   L.Matrix Double ->
   -- Estimate of mu.
   Double ->
   Double
-d2E sigma mu = undefined
+d2E im sigma mu = frobenius m m
+  where
+    m = sigma - L.scale mu im
 
--- Estimator of b2 (lemma 3.4).
+-- Estimator of b2 (Lemma 3.4).
 b2E ::
   -- Data matrix.
   L.Matrix Double ->
@@ -66,9 +81,22 @@ b2E ::
   -- Estimate of d2.
   Double ->
   Double
-b2E xs sigma d2 = undefined
+b2E xs sigma = min b2m
+  where
+    n = fromIntegral $ L.rows sigma
+    -- NOTE: The authors use a transposed data matrix. They refer to one out of
+    -- n columns, each with p rows. Here, we have one out of n rows, each with p
+    -- columns.
+    --
+    -- Long story short. Each y is an observation, a vector of length p.
+    ys =
+      [ frobenius d d
+        | y <- L.toRows xs,
+          let d = (L.asColumn y L.<> L.asRow y) - sigma
+      ]
+    b2m = recip (n * n) * foldl' (+) 0 ys
 
--- Estimator of a2 (lemma 3.5).
+-- Estimator of a2 (Lemma 3.5).
 a2E ::
   -- Estimate of d2.
   Double ->
@@ -91,4 +119,4 @@ covariance' ::
   -- Estimate of a2.
   Double ->
   L.Matrix Double
-covariance' iN sigma mu d2 b2 a2 = L.scale (b2 / d2 * mu) iN + L.scale (a2 / d2) sigma
+covariance' im sigma mu d2 b2 a2 = L.scale (b2 / d2 * mu) im + L.scale (a2 / d2) sigma
