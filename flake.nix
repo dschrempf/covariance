da0ac897801977b8157165f178c0ff06f79506d2
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-02-17 17:38:52 +0100

    ghc 921

--- flake.nix
+++ flake.nix
@@ -10,41 +10,41 @@
     , flake-utils
     , nixpkgs
     }:
-      flake-utils.lib.eachDefaultSystem (
-        system:
-          let
-            # haskell-overlay = (
-            #   selfn: supern: {
-            #     haskellPackages = supern.haskellPackages.override {
-            #       overrides = selfh: superh: {};
-            #     };
-            #   }
-            # );
-            # overlays = [ ];
-            pkgs = import nixpkgs {
-              inherit system;
-            };
-            hpkgs = pkgs.haskellPackages;
-            hlib = pkgs.haskell.lib;
-            covariance = hpkgs.callCabal2nix "covariance" ./. rec {};
-            covariance-dev = hlib.doBenchmark covariance;
-          in
-            {
-              packages.covariance = covariance;
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # haskell-overlay = (
+        #   selfn: supern: {
+        #     haskellPackages = supern.haskellPackages.override {
+        #       overrides = selfh: superh: {};
+        #     };
+        #   }
+        # );
+        # overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        hpkgs = pkgs.haskell.packages.ghc921;
+        hlib = pkgs.haskell.lib;
+        covariance = hpkgs.callCabal2nix "covariance" ./. rec { };
+        covariance-dev = hlib.doBenchmark covariance;
+      in
+      {
+        packages.covariance = covariance;
 
-              defaultPackage = covariance;
+        defaultPackage = covariance;
 
-              devShell = hpkgs.shellFor {
-                packages = _: [ covariance-dev ];
-                buildInputs = with pkgs; [
-                  # See https://github.com/NixOS/nixpkgs/issues/59209.
-                  bashInteractive
-                  hpkgs.cabal-install
-                  hpkgs.haskell-language-server
-                ];
-                doBenchmark = true;
-                withHoogle = true;
-              };
-            }
-      );
+        devShell = hpkgs.shellFor {
+          packages = _: [ covariance-dev ];
+          buildInputs = with pkgs; [
+            # See https://github.com/NixOS/nixpkgs/issues/59209.
+            bashInteractive
+            hpkgs.cabal-install
+            hpkgs.haskell-language-server
+          ];
+          doBenchmark = true;
+          withHoogle = true;
+        };
+      }
+    );
 }
