29b5fb0ce566e514805e0de89a1ba501054bf9ec
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-03-07 17:08:39 +0100

    update (stackage nightly)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/haskell-updates";
 
   outputs =
     { self
@@ -43,7 +43,7 @@
             hpkgs.haskell-language-server
           ];
           doBenchmark = true;
-          withHoogle = true;
+          # withHoogle = true;
         };
       }
     );
