9ab74f70f3c0bd051ba97bda2ba0a737141dea68
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-06-09 14:20:12 +0200

    ghc923

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        hpkgs = pkgs.haskell.packages.ghc922;
+        hpkgs = pkgs.haskell.packages.ghc923;
         hlib = pkgs.haskell.lib;
         covariance = hpkgs.callCabal2nix "covariance" ./. rec { };
         covariance-dev = hlib.doBenchmark covariance;
