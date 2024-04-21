fd352126b16b6983589dd61d747f7b659d53d8c6
Dominik Schrempf <dominik.schrempf@gmail.com>
2024-04-21 06:46:07 +0200

    tooling

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       theseHpkgNames = [
         "covariance"
       ];
-      thisGhcVersion = "ghc92";
+      thisGhcVersion = "ghc96";
       hOverlay = selfn: supern: {
         haskell = supern.haskell // {
           packageOverrides = selfh: superh:
